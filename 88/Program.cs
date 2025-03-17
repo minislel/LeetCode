22ab23c6b54f93873570e67bc1dfbadfa775480a
minislel <marcin4kt@gmail.com>
2025-03-17 19:02:26 +0100

    fixed 88

--- 88/Program.cs
+++ 88/Program.cs
@@ -1,39 +1,43 @@
-﻿
+﻿public class Program
+{
+    public static void Main()
+    {
+        var nums1 = new int[] { 4,5,6,0,0,0};
+        var nums2 = new int[] { 1,2,3};
+        var solution = new Solution();
+        solution.Merge(nums1, 3, nums2, 3);
+        foreach (var num in nums1)
+        {
+            Console.WriteLine(num);
+        }
+    } 
+}
 public class Solution
 {
     public void Merge(int[] nums1, int m, int[] nums2, int n)
     {
-        if (n == 0)
+        int A = m - 1;
+        int B = n - 1;
+        int C = m+n-1;
+        while (A>=0 && B>=0)
         {
-            return;
-        }
-        if (m == 0)
-        { 
-            for (int i = 0; i < n; i++)
+            if (nums1[A] > nums2[B])
             {
-                nums1[i] = nums2[i];
+                nums1[C] = nums1[A];
+                A--;
             }
-            return;
+            else
+            {
+                nums1[C] = nums2[B];
+                B--;
+            }
+            C--;
         }
-        for (int i = m + n - 1; i >= 0; i--)
+        while (B>=0)
         {
-            if (nums1[i] == 0 && nums1[i - 1] == 0)
-            {
-                nums1[i] = nums2[n - 1];
-                n--;
-            }
-            else if (nums1[i] == 0 && nums1[i - 1] != 0 && nums2[n - 1] < nums1[i - 1])
-            {
-                nums1[i] = nums1[i-1];
-                nums1[i-1] = nums2[n - 1];
-                n--;
-            }
-            else if (nums1[i] == 0 && nums1[i - 1] != 0 && nums2[n - 1] >= nums1[i - 1])
-            {
-                nums1[i] = nums2[n - 1];
-                n--;
-            }
-            
+            nums1[C] = nums2[B];
+            B--;
+            C--;
         }
     }
 }
