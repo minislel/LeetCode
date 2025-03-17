21ca17b73235e8de94f04621aeb9183c8e55ab76
minislel <marcin4kt@gmail.com>
2025-03-17 20:32:57 +0100

    completed 1

---
+++ 1/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int L = 0;
+        int R = nums.Length - 1;
+        var sorted = nums.OrderBy(x => x).ToArray();
+        while (true)
+        {
+            if (sorted[L] + sorted[R] > target)
+            {
+                R--;
+            }
+            else if (sorted[L] + sorted[R] < target)
+            {
+                L++;
+            }
+            else
+            {
+                R = Array.LastIndexOf(nums, sorted[R]);
+                L = Array.IndexOf(nums, sorted[L]);
+                return new int[] { L, R };
+            }
+        }
+    }
+}
