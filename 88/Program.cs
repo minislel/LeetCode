96ede69719bc7941e657f33f03ad572a09365363
minislel <marcin4kt@gmail.com>
2025-03-17 18:23:58 +0100

    Add project files.

---
+++ 88/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        if (n == 0)
+        {
+            return;
+        }
+        if (m == 0)
+        { 
+            for (int i = 0; i < n; i++)
+            {
+                nums1[i] = nums2[i];
+            }
+            return;
+        }
+        for (int i = m + n - 1; i >= 0; i--)
+        {
+            if (nums1[i] == 0 && nums1[i - 1] == 0)
+            {
+                nums1[i] = nums2[n - 1];
+                n--;
+            }
+            else if (nums1[i] == 0 && nums1[i - 1] != 0 && nums2[n - 1] < nums1[i - 1])
+            {
+                nums1[i] = nums1[i-1];
+                nums1[i-1] = nums2[n - 1];
+                n--;
+            }
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# LeetCode Exercises
+This is my repository of LeetCode Exercises
+My list of completed exercises:
+
+ [88. Merge Sorted Array](https://leetcode.com/problems/merge-sorted-array/)
