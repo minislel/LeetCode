a23d8e2d7c921d0ba506a42adea8c1f858acf00a
minislel <marcin4kt@gmail.com>
2025-03-18 21:43:20 +0100

    completed 26

---
+++ 26/Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace _26
+{
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int L = 0;
+            for (int R = 1; R < nums.Length; R++)
+            {
+                if (nums[L] != nums[R])
+                { 
+                    L++;
+                    nums[L] = nums[R];
+                }
+            }
+            return L + 1;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,6 +5,8 @@ This is my repository of LeetCode Exercises
 My list of completed exercises:
 
  [1. Two Sum](https://leetcode.com/problems/two-sum/)
+ 
+ [26. Remove Duplicates from Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array/))
 
  [27. Remove Element](https://leetcode.com/problems/remove-element/)
 
