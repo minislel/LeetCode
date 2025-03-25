ab02951e53586a1cd1e652c180faf27e6801fb39
minislel <marcin4kt@gmail.com>
2025-03-25 14:36:42 +0100

    completed 13

---
+++ 13/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.RomanToInt("III"));
+            Console.WriteLine(solution.RomanToInt("IV"));
+            Console.WriteLine(solution.RomanToInt("IX"));
+            Console.WriteLine(solution.RomanToInt("LVIII"));
+            Console.WriteLine(solution.RomanToInt("MCMXCIV"));
+        }
+    }
+    public class Solution
+    {
+        private Dictionary<char, int> romanMap = new Dictionary<char, int>
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000},
+        };
+        public int RomanToInt(string s)
+        {
+            int result = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (i >=1 && romanMap[s[i]] > romanMap[s[i-1]])
+                {
+                    result += romanMap[s[i]] - romanMap[s[i - 1]];
+                    i--;
+                }
+                else
+                {
+                    result += romanMap[s[i]];
+                }
+            }
+            return result;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,6 +5,8 @@ This is my repository of LeetCode Exercises
 My list of completed exercises:
 
  [1. Two Sum](https://leetcode.com/problems/two-sum/)
+
+ [13. Roman to Integer](https://leetcode.com/problems/roman-to-integer/)
  
  [26. Remove Duplicates from Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array/))
 
