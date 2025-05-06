1cbc36e2819ff1115e2fa9e156fe3a7fb440ec0b
minislel <marcin4kt@gmail.com>
2025-05-07 00:01:48 +0200

    Completed 2. Add Two Numbers

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,10 +1,66 @@
-﻿namespace _2
+﻿using System.Numerics;
+
+namespace _2
 {
-    internal class Program
+    public class ListNode
     {
-        static void Main(string[] args)
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
         {
-            Console.WriteLine("Hello, World!");
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            string num1 = "";
+            string num2 = "";
+            string num1reverse = "";
+            string num2reverse = "";
+
+            while (l1 is not null)
+            {
+                num1 += l1.val.ToString();
+                l1 = l1.next;
+            }
+            while (l2 is not null)
+            {
+                num2 += l2.val.ToString();
+                l2 = l2.next;
+            }
+            foreach (var c in num1.Reverse())
+            {
+                num1reverse += c.ToString();
+            }
+            foreach (var c in num2.Reverse())
+            {
+                num2reverse += c.ToString();
+            }
+            BigInteger n1 = BigInteger.Parse(num1reverse);
+            BigInteger n2 = BigInteger.Parse(num2reverse);
+            BigInteger sum = n1 + n2;
+
+            List<ListNode> nodes = new List<ListNode>();
+            for (int i = sum.ToString().Length-1; i >= 0; i--)
+            {
+
+                ListNode node = new ListNode();
+                var temp = int.Parse(sum.ToString("F")[i].ToString());
+                node.val = temp;
+                nodes.Add(node);
+            }
+            for (int i = 0; i < nodes.Count - 1; i++)
+            {
+                if (i < nodes.Count-1)
+                {
+                    nodes[i].next = nodes[i + 1];
+                }
+            }
+            return nodes[0];
         }
     }
 }
--- README.md
+++ README.md
@@ -6,6 +6,8 @@ My list of completed exercises:
 
  [1. Two Sum](https://leetcode.com/problems/two-sum/)
 
+ [2. Add Two Numbers](https://leetcode.com/problems/add-two-numbers/)
+
  [13. Roman to Integer](https://leetcode.com/problems/roman-to-integer/)
  
  [26. Remove Duplicates from Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array/))
