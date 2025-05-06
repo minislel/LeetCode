a404ef696ef737091a2d262608b351ea9d62b094
minislel <marcin4kt@gmail.com>
2025-05-06 23:25:12 +0200

    .

---
+++ 189/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _189
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public class Solution
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            int L = 0;
+            int R = nums.Length - 1;
+            int[] numsCopy = new int[nums.Length];
+            Array.Copy(nums, numsCopy, nums.Length);
+            k %= nums.Length;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[L] = nums[R];
+                L++;
+            }
+        }
+    }
+}
