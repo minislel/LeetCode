fa8e1978f962b8e5888e74425413d7c0b01629b4
minislel <marcin4kt@gmail.com>
2025-08-05 20:26:04 -0400

    completed 3

---
+++ 3/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            var result = solution.LengthOfLongestSubstring("abcabcbb");
+            Console.WriteLine(result);
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var chars = new List<char>();
+            int maxLength = 0;  
+            int L = 0, R = 0;
+            while (R < s.Length)
+            {
+                if (!chars.Contains(s[R]))
+                {   
+                    if (R - L + 1 > maxLength)
+                    {
+                        maxLength = R - L + 1;
+                    }
+                    chars.Add(s[R]);
+                    R++;
+                }
+                else
+                {
+                    while (L < R && chars.Contains(s[R]))
+                    {
+                        chars.Remove(s[L]);
+                        L++;
+                    }
+                }
+            }
+            return maxLength;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -8,6 +8,8 @@ My list of completed exercises:
 
  [2. Add Two Numbers](https://leetcode.com/problems/add-two-numbers/)
 
+ [3. Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)
+
  [13. Roman to Integer](https://leetcode.com/problems/roman-to-integer/)
 
  [14. Longest Common Prefix](https://leetcode.com/problems/longest-common-prefix/)
